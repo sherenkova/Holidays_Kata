adcb232df6d7fcab9b24edaf2920073c056f5717
Nastassia <nastassiasherenkova@ied.edu>
2016-02-25 23:42:30 +0100

    added parses in constructor
    added parses in constructor

--- src/code/DatesUtility.java
+++ src/code/DatesUtility.java
@@ -1,5 +1,18 @@
 package code;
 
-public class DatesUtility {
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
+public class DatesUtility {
+	Date startDate;
+	Date endDate;
+	public DatesUtility(String date1, String date2) throws ParseException
+	{
+		DateFormat df = new SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH);
+	    Date d1 = df.parse(date1);
+		Date d2 = df.parse(date2);
+	}
 }
--- src/tests/DatesUtilityTests.java
+++ src/tests/DatesUtilityTests.java
@@ -2,12 +2,16 @@ package tests;
 
 import static org.junit.Assert.*;
 
+import java.text.ParseException;
+
 import org.junit.Test;
+
+import code.DatesUtility;
 
 public class DatesUtilityTests {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testConstructor() throws ParseException {
+		DatesUtility util = new DatesUtility("29/02/2016", "06/12/2016");
 	}
 }
